0d3204f9adb76df143e0cd5541cbc9b8cf1047e7
dudzio <dud2iol@gmail.com>
2018-03-28 18:27:15 +0200

    functionality added: adding new todos +effects fixes

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,11 +1,21 @@
 // Check off Specific Todos By Clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 })
 //Click on X to delete Todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+})
+// Hit Enter and Add Todo
+$("input").keypress(function(event){
+	if(event.which === 13){
+		var todoText = $(this).val();
+		//create a new li, add to ul with todoText
+		$("ul").append("<li><span>X </span>"+todoText+"</li>");
+		//clear input field
+		$(this).val("");
+	}
 })
