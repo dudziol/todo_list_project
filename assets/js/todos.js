126b9d76f228e81628ee54de54873c9f6ce78832
dudzio <dud2iol@gmail.com>
2018-03-28 18:13:10 +0200

    functionality: deleting lis

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,10 @@
 $("li").click(function(){
 	$(this).toggleClass("completed");
 })
+//Click on X to delete Todo
+$("span").click(function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
